2eef9f070e2979b2b1c7586b61050de2b8c1b66c
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-11-20 21:51:42 -0300

    Added file parsing for carts

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import argparse
 import database_connections as dbc
 
+FILENAME = "./data/carts.csv"
+
+
+# Function to read the carts data from the files
+def read_carts(filename):
+    f = open(filename, 'r')
+
+    # Extract data
+    data = []
+    for line in f:
+        d = line.rstrip("\n").split(";")
+        data.append([int(d[0]), int(d[1]), int(d[2])])
+    return data
+
+
 # main() function
 def main():
     # Command line args are in sys.argv[1], sys.argv[2] ..
@@ -20,6 +35,7 @@ def main():
     p = dbc.PostgresConnection()
     r.insert_data(1, 2, 3)
     p.insert_data(1, 2, 3)
+
 
 # call main
 if __name__ == '__main__':
