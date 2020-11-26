de51db0b53edf575cc7dd703b4f06827637705b9
nicbritos <nicbritos@hotmail.com>
2020-11-25 22:20:18 -0300

    Added methods to insert users and products

--- utils/database_connections.py
+++ utils/database_connections.py
@@ -23,15 +23,27 @@ class PostgresConnection:
         cur.execute("""
             CREATE TABLE IF NOT EXISTS users (
                 id INT PRIMARY KEY,
-                full_name VARCHAR(255) NOT NULL
+                full_name TEXT NOT NULL
             );
+            
+            create sequence IF NOT EXISTS users_id_seq;
+            
+            alter table users alter column id set default nextval('public.users_id_seq');
+            
+            alter sequence users_id_seq owned by users.id;
             
             CREATE TABLE IF NOT EXISTS products (
                 id INT PRIMARY KEY,
                 title text NOT NULL,
                 description text NOT NULL,
-                price int NOT NULL
+                price INT NOT NULL
             );
+            
+            create sequence if not exists products_id_seq;
+            
+            alter table products alter column id set default nextval('public.products_id_seq');
+            
+            alter sequence products_id_seq owned by products.id;
             
             CREATE TABLE IF NOT EXISTS carts (
                 product_id INT,
@@ -44,13 +56,39 @@ class PostgresConnection:
             );
         """)
         self.con.commit()
+        cur.close()
+
+    def insert_user(self, full_name, id = None):
+        cur = self.con.cursor()
+
+        if id is None:
+            cur.execute("INSERT INTO users (full_name) VALUES (%s);", full_name)
+        else:
+            cur.execute("INSERT INTO users (id, full_name) VALUES (%s, %s);", (id, full_name))
+
+        self.con.commit()
+        cur.close()
+
+    def insert_product(self, title, description, price, id = None):
+        cur = self.con.cursor()
+
+        if id is None:
+            cur.execute("INSERT INTO products (title, description, price) VALUES (%s, %s, %s);",
+                        (title, description, price))
+        else:
+            cur.execute("INSERT INTO products (id, title, description, price) VALUES (%s, %s, %s, %s);",
+                        (id, title, description, price))
+
+        self.con.commit()
+        cur.close()
 
     # Method to insert data into the carts table
-    def insert_data(self, user_id, product_id, quantity):
+    def insert_cart(self, user_id, product_id, quantity):
         cur = self.con.cursor()
         cur.execute("INSERT INTO carts (product_id, user_id, quantity) VALUES (%s, %s, %s);",
                     (product_id, user_id, quantity))
         self.con.commit()
+        cur.close()
 
     # Clears the carts table
     def delete_all(self):
